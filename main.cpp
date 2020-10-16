e6a027dd5134372c6573d2de157dfb5c8d409739
dashamasha <72272849+dashamasha@users.noreply.github.com>
2020-10-16 18:41:56 +0500

    Laba222

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // biblioteka
+
+double Laba2 =  40'000'000'000.0f;
+
+
+int main()
+{
+  using namespace std;
+ 
+  cout << Laba2 << endl;
+  Laba2 = Laba2 + 1.0f;
+  cout << Laba2 << endl;
+  return 0;
+}
